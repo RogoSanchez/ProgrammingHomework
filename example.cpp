681d1004b709dc7d38a9985b49cccadad3dcee8f
Jhoan <156856925+okamikin287@users.noreply.github.com>
2024-07-10 22:08:32 -0400

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,23 @@
+//ejemplo de uso
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include "Book.h"
+#include "Library.cpp"
+
+int main(){
+	Book b1, b2(45, "XD", "asd", 2), b3;
+	Library a;
+	
+	cin>>b1>>b3;
+	
+	a.Insert(b1);
+	a.Insert(b3);
+	a.Insert(4, "Deitel", "How to Program", 4);
+	a.Insert(b2);
+	
+	a.Remove("XD", "asd");
+	
+	a.Show();
+}
