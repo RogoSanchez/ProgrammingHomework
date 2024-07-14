912c2fc4be95cbf282ce8d404cdade9381440e4a
Roberto Gallardo Sanchez <rogo.sanchez1@gmail.com>
2024-07-14 00:33:40 -0400

    Update Interfaz.cpp

--- Interfaz.cpp
+++ Interfaz.cpp
@@ -3,18 +3,35 @@
 #include <iostream> 
 using namespace std;
 
+
+/*!
+    *  \brief //Implementation of the Interface of the Library class
+    *  \author    Roberto Gallardo Sanchez
+    *  \version   1.0
+    *  \date      2024-07-09
+    *  \return  none
+    *  \pre none
+    
+  */
 int main (){
     Library Nacional;
     int Option=10;
     while (Option!=0)
-    {
-        cout<<"Press #1 to insert a book\n";
-        cout<<"Press #2 to inform lost\n";
-        cout<<"Press #3 to show all the books of any author\n";
-        cout<<"Press #4 to find a book for his code\n";
-        cout<<"Press #5 for loan a book\n";
-        cout<<"Press #6 to find a book for keywords\n";
-        cout<<"Press #0 to exit\n";
+    {   
+        cout<<" ----------------------------GOOD MORNING PEOPLE, WELCOME TO OUR LIBRARY APP----------------------------------\n";
+        cout<<"--------------------------------------------------------------------------------------------------------------\n";
+        cout<<"----> To insert a book ------->           Press #1\n";
+        cout<<"----> To inform lost---------->           Press #2\n";
+        cout<<"----> For loan a book--------->           Press #3 \n";
+        cout<<"----> To find a book for his code--->     Press #4 \n";
+        cout<<"----> To show the books of any author->   Press #5 \n";
+        cout<<"----> To find a book for keywords--->     Press #6 \n";
+        cout<<"--------------------------------------------------------------------------------------------------------------\n";
+        cout<<"                                                                                                              \n";
+        cout<<"                     To exit----->Press #0 \n";
+        cout<<"                                                                                                              \n";
+        cout<<"--------------------------------------------------------------------------------------------------------------\n";
+        cout<<"                © 2004 - 2024 All Copyrights reserved \n";
         cin>>Option;
         if (Option==1)
         {   Book Insertar;
@@ -24,14 +41,16 @@ int main (){
         if (Option==2)
         {   string Author;
             string Title;     
+            fflush(stdin);
             cout<<"Provide us a Title :\n";
             getline(cin,Title);
             cout<<"Provide us an Author :\n";
             getline(cin,Author);
             Nacional.Remove(Title, Author);
         }
-        if(Option==3){
+        if(Option==5){
             string Author;
+            fflush(stdin);
             cout<<"Provide us an Author :\n";
             getline(cin,Author);
             Nacional.ShowBooks(Author);
@@ -42,9 +61,10 @@ int main (){
             cin>>Code;
             Nacional.FindBook(Code);
         }
-        if (Option==5)
+        if (Option==3)
         {   string Author;
             string Title;     
+            fflush(stdin);
             cout<<"Provide us a Title :\n";
             getline(cin,Title);
             cout<<"Provide us an Author :\n";
@@ -54,6 +74,7 @@ int main (){
         if (Option==6)
         {   
             string Keyword;
+            fflush(stdin);
             cout<<"Provide us a Keyword :\n";
             getline(cin,Keyword);
             //Nacional.ShowKey(Keyword);
